08fd8ce33aa6cd9bff2083d94fd8eb7a423e5336
shirishamarolix <136450664+shirishamarolix@users.noreply.github.com>
2023-08-22 18:39:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,11 +11,16 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "instance1" {
-    ami = "ami-0da59f1af71ea4ad2"
-    instance_type = "t2.micro"
+resource "aws_s3_bucket" "bucket2" {
+  bucket = "my-tf-siribuck227713"
+  versioning {
+        enabled = "true"
+  }
+  tags = {
+    Name        = "siribuck22"
+    Environment = "Dev"
+  }
 }
-
   
    
 
