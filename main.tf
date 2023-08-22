21c3261a92500fdabcb3df10289f84c0f568942f
harikrishnamarolix <135110098+harikrishnamarolix@users.noreply.github.com>
2023-08-22 15:46:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,12 +12,20 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "bucket2" {
-  bucket = "my-tf-samplebucket34234"
+  bucket = "my-tf-samplebucket342345"
   versioning {
         enabled = "true"
   }
   tags = {
-    Name        = "haribucket1234678"
+    Name        = "haribucket123467"
     Environment = "Dev"
+  }
+}
+resource "aws_ebs_volume" "volume1" {
+  availability_zone = "us-east-2a"
+  size              = 10
+
+  tags = {
+    Name = "Ebs-Volume"
   }
 }
