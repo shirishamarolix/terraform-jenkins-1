8ca1278b256bea91d9252fbc7e1daab60c585ce9
shirishamarolix <136450664+shirishamarolix@users.noreply.github.com>
2023-08-23 10:55:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,9 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_ebs_volume" "example2213" {
-  availability_zone = "ap-south-1"
-  size              = 8
+resource "aws_ebs_volume" "siriram2213" {
+  availability_zone = 'ap-south-1'
+  size              = 10
 
   tags = {
     Name = "Hellokurnool"
