0be2005336aae7b6e01a4dab88c392b1cdbea679
shirishamarolix <136450664+shirishamarolix@users.noreply.github.com>
2023-08-23 09:58:24 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_vpc" "main" {
     enable_dns_support = true
     enable_dns_hostnames = true
   tags = {
-    Name = "dhanu01"
+    Name = "siri01"
     Terraform = "true"
     Environment = "DEV"
   }
